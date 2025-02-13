77fd6dcf91635032531d6f898c1e9440e3183828
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-13 15:42:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-//complete this code
-class Animal {}
+class Animal {
+	 constructor(species) {
+        this.species = species;  
+    }
+	get species (){
+		return this._species
+	}
+	set species(value) {
+        this._species = value; 
+    }
+	makeSound(){
+		console.log(`The ${this.species} makes a sound`)
+	}
+}
+// let ani=new Animal('spacime')
+// console.log(ani.makeSound())
 
-class Dog extends Animal {}
+class Dog extends Animal {
+    constructor(species) {
+        super(species)
+    }
+    bark(){
+        console.log('woof')
+    }
+}
+// let myDog=new Dog('golden Retriver')
+// myDog.makeSound()
 
-class Cat extends Animal {}
+class Cat extends Animal {
+    constructor(species) {
+        super(species)
+    }
+    purr(){
+        console.log('purr')
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
